a328449aae6193d866a8a5d8e95a31c8ff0c9f39
vipor <sokvipor123@gmail.com>
2024-12-30 21:54:49 +0700

    assignment week10

--- Graph.hpp
+++ Graph.hpp
@@ -1,1 +1,64 @@
+#ifndef GRAPH_H
+#define GRAPH_H
 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+class Graph {
+private:
+
+    int V;
+    std::vector<std::vector<int>> adj;
+
+public:
+    Graph(int V) : V(V), adj(V) {}
+
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+    }
+
+    void DFS(int s) {
+        std::vector<bool> visited(V, false);
+        std::stack<int> stack;
+        stack.push(s);
+        visited[s] = true;
+
+        while (!stack.empty()) {
+            int u = stack.top();
+            stack.pop();
+            std::cout << u << " ";
+
+            for (int v : adj[u]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    stack.push(v);
+                }
+            }
+        }
+    }
+
+    void BFS(int s) {
+        std::vector<bool> visited(V, false);
+        std::queue<int> queue;
+        queue.push(s);
+        visited[s] = true;
+
+        while (!queue.empty()) {
+            int u = queue.front();
+            queue.pop();
+            std::cout << u << " ";
+
+            for (int v : adj[u]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    queue.push(v);
+                }
+            }
+        }
+    }
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include "Graph.hpp"
 
+
+int main() {
+    int V = 5;
+    Graph g(V);
+
+    g.addEdge(0, 1);
+    g.addEdge(0, 4);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(3, 4);
+
+    std::cout << "Depth First Traversal (starting from vertex 0): ";
+    g.DFS(0);
+    std::cout << std::endl;
+
+    std::cout << "Breadth First Traversal (starting from vertex 0): ";
+    g.BFS(0);
+    std::cout << std::endl;
+
+    return 0;
+}
