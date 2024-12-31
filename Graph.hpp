1fb735e72ea9f0814582ed0cb86730077f2265cf
vipor <sokvipor123@gmail.com>
2024-12-31 16:21:37 +0700

    edit assignment week10

--- Graph.hpp
+++ Graph.hpp
@@ -3,21 +3,35 @@
 
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <queue>
+#include <list>
 #include <stack>
+#include <queue>
+
+using namespace std;
 
 class Graph {
 private:
-
-    int V;
-    std::vector<std::vector<int>> adj;
+    int V;  
+    std::vector<std::list<int>> adj; 
 
 public:
+    
     Graph(int V) : V(V), adj(V) {}
 
     void addEdge(int u, int v) {
         adj[u].push_back(v);
+        adj[v].push_back(u);  
+    }
+
+    void Display() {
+        std::cout << "Graph Structure (Adjacency List):" << std::endl;
+        for (int i = 0; i < V; ++i) {
+            std::cout << i << ": ";
+            for (int neighbor : adj[i]) {
+                std::cout << neighbor << " ";
+            }
+            std::cout << std::endl;
+        }
     }
 
     void DFS(int s) {
@@ -26,18 +40,20 @@ public:
         stack.push(s);
         visited[s] = true;
 
+        std::cout << "Depth-First Search (DFS) starting from vertex 0: ";
         while (!stack.empty()) {
             int u = stack.top();
             stack.pop();
             std::cout << u << " ";
 
-            for (int v : adj[u]) {
-                if (!visited[v]) {
-                    visited[v] = true;
-                    stack.push(v);
+            for (auto it = adj[u].rbegin(); it != adj[u].rend(); ++it) {  // Iterate in reverse order for consistent output
+                if (!visited[*it]) {
+                    visited[*it] = true;
+                    stack.push(*it);
                 }
             }
         }
+        std::cout << std::endl;
     }
 
     void BFS(int s) {
@@ -46,6 +62,7 @@ public:
         queue.push(s);
         visited[s] = true;
 
+        std::cout << "Breadth-First Search (BFS) starting from vertex 0: ";
         while (!queue.empty()) {
             int u = queue.front();
             queue.pop();
@@ -58,6 +75,7 @@ public:
                 }
             }
         }
+        std::cout << std::endl;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include "Graph.hpp"
 
-
 int main() {
-    int V = 5;
-    Graph g(V);
+    Graph g(5);
 
     g.addEdge(0, 1);
     g.addEdge(0, 4);
@@ -12,13 +10,10 @@ int main() {
     g.addEdge(1, 3);
     g.addEdge(3, 4);
 
-    std::cout << "Depth First Traversal (starting from vertex 0): ";
-    g.DFS(0);
-    std::cout << std::endl;
+    g.Display();
 
-    std::cout << "Breadth First Traversal (starting from vertex 0): ";
+    g.DFS(0);
     g.BFS(0);
-    std::cout << std::endl;
 
     return 0;
 }
